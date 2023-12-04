b1d38cadb7ab8e6b04243df6d583421eb18d0a75
Albina <62645685+olbenka@users.noreply.github.com>
2023-12-04 18:11:08 +0300

    15: second try

--- lesson8/src/fifteen/VetClinicApp.java
+++ lesson8/src/fifteen/VetClinicApp.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static fifteen.WorkWithDB.*;
+import static fifteen.DbCreator.*;
 
 public class VetClinicApp {
     public static void main(String[] args) {
